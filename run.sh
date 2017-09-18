0aa6da2a07e3d6e694761194deed6f771d2be1fa
Kangze Huang <kangzeroo@gmail.com>
2017-09-18 02:26:50 -0400

    fixed dockerfile to right port

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -d -it -p 8080:8080 --name=rentburrowtenant rentburrowtenant npm run start -- --host=0.0.0.0
+docker run -d -it -p 8081:8081 --name=rentburrowtenant rentburrowtenant npm run start -- --host=54.145.200.208
