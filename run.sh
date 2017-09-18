8c14c13bad687580346875351482104cac6cdd1f
Kangze Huang <kangzeroo@gmail.com>
2017-09-18 12:40:23 -0400

    for prod

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -d -it -p 8081:8081 --name=tenant_frontend tenant_frontend npm run node -- --host=54.145.200.208
+docker run -d -it -p 8081:8081 --name=tenant_frontend tenant_frontend npm run node -- --host=54.162.169.7
