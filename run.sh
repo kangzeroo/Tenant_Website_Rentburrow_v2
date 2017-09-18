3b7b461b9f17dde8f2e6425e492c0a23f9e07731
Kangze Huang <kangzeroo@gmail.com>
2017-09-18 02:28:07 -0400

    fixed dockerfile to right port

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -d -it -p 8081:8081 --name=rentburrowtenant rentburrowtenant npm run start -- --host=54.145.200.208
+docker run -d -it -p 8081:8081 --name=tenant_frontend tenant_frontend npm run node -- --host=54.145.200.208
