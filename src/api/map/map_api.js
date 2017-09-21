81119427beff031683c3ce96b26bf8c4cf4d6b40
Kangze Huang <kangzeroo@gmail.com>
2017-09-21 12:27:25 -0400

    sublet bouncing is working

--- src/api/map/map_api.js
+++ src/api/map/map_api.js
@@ -39,11 +39,19 @@ export const findAllMatchingGPS = (building, all_buildings) => {
 
 // return the Pin_ID of the pin with GPS coords matching the Pin passed in
 // FIX THIS
-export const matchPinIDFromPins = (marker, pins) => {
-	console.log(marker)
-	console.log(marker.position)
-	console.log(marker.position.lat().toFixed(7), marker.position.lng().toFixed(7))
-	return pins.filter((pin) => {
-		return pin.position.lat().toFixed(7) === marker.position.lat().toFixed(7) && pin.position.lng().toFixed(7) === marker.position.lng().toFixed(7)
+export const matchPinIDFromPins = (pin_id, pins, listOfResults) => {
+	let match
+	listOfResults.forEach((building) => {
+		if (building.post_id === pin_id) {
+			match = building
+		}
 	})
+	const sames = pins.filter((pin) => {
+		return pin.position.lat().toFixed(7) === match.gps_x.toFixed(7) && pin.position.lng().toFixed(7) === match.gps_y.toFixed(7)
+	})
+	if (sames[0] && sames[0].pin_id) {
+		return sames[0].pin_id
+	} else {
+		return pin_id
+	}
 }
