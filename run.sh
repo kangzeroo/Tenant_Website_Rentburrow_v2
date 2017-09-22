c44563d0481a2c7f582633298d15b06f84414b37
Kangze Huang <kangzeroo@gmail.com>
2017-09-22 05:38:48 -0400

    added ssl support

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -d -it -p 80:8081 --name=tenant_frontend tenant_frontend npm run prod -- --host=34.195.86.132
+docker run -d -it -p 433:8081 --name=tenant_frontend tenant_frontend npm run prod -- --host=34.195.86.132
