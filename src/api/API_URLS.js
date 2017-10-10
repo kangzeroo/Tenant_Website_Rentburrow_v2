958856ee608bdc9b565a8e86f269613d0dfa76e6
Jimmy <j63guo@uwaterloo.ca>
2017-10-10 18:12:43 -0400

    Asd

--- src/api/API_URLS.js
+++ src/api/API_URLS.js
@@ -11,11 +11,12 @@ export const FB_APP_ID = '115765432474914'
 // export const GENERIC_MICROSERVICE = 'https://localhost:3001'
 // export const FB_PARSER_MICROSERVICE = 'https://localhost:3001'
 export const INTEL_MICROSERVICE = 'https://localhost:3004'
+export const SUBLETTING_MICROSERVICE = 'https://localhost:3005'
 
 // // PROD
 export const MESSAGING_MICROSERVICE = 'https://rentburrow.com:3001'
 // export const FB_APP_ID = '1492022030811505'
-
+//
 export const SEARCH_MICROSERVICE = 'https://rentburrow.com:3002'
 export const CORP_MICROSERVICE = 'https://rentburrow.com:3001'
 export const REGISTER_MICROSERVICE = 'https://rentburrow.com:3001'
