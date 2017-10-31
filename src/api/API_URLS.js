91a5c60e45c9700463e692e8f3f26d1590511680
Kangze Huang <kangzeroo@gmail.com>
2017-10-31 11:45:52 -0400

    multiple vr tours supported

--- src/api/API_URLS.js
+++ src/api/API_URLS.js
@@ -3,9 +3,9 @@
 
 // LOCAL
 // export const MESSAGING_MICROSERVICE = 'https://localhost:3001'
-export const FB_APP_ID = '115765432474914'
+// export const FB_APP_ID = '115765432474914'
 
-export const SEARCH_MICROSERVICE = 'https://localhost:3002'
+// export const SEARCH_MICROSERVICE = 'https://localhost:3002'
 // export const CORP_MICROSERVICE = 'https://localhost:3001'
 // export const REGISTER_MICROSERVICE = 'https://localhost:3001'
 // export const GENERIC_MICROSERVICE = 'https://localhost:3001'
@@ -18,9 +18,9 @@ export const SEARCH_MICROSERVICE = 'https://localhost:3002'
 
 // // PROD
 export const MESSAGING_MICROSERVICE = 'https://rentburrow.com:3001'
-// export const FB_APP_ID = '1492022030811505'
+export const FB_APP_ID = '1492022030811505'
 
-// export const SEARCH_MICROSERVICE = 'https://rentburrow.com:3002'
+export const SEARCH_MICROSERVICE = 'https://rentburrow.com:3002'
 export const CORP_MICROSERVICE = 'https://rentburrow.com:3001'
 export const REGISTER_MICROSERVICE = 'https://rentburrow.com:3001'
 export const GENERIC_MICROSERVICE = 'https://rentburrow.com:3001'
