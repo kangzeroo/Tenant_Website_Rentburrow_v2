dfc21ace1910f7c0dbb2b9a0fc918f1c09e29fc5
Jimmy <j63guo@uwaterloo.ca>
2017-12-03 05:41:09 -0500

    asd

--- src/api/auth/register_api.js
+++ src/api/auth/register_api.js
@@ -1,19 +1,19 @@
 import axios from 'axios'
-import { REGISTER_MICROSERVICE } from '../API_URLS'
+import { CONTRACTING_MICROSERVICE } from '../API_URLS'
 import authHeaders from '../authHeaders'
 
 // save the registration info to server and await for response
-export const sendRegisterInfo = ({ tenant_id, email, name, phone }) => {
+export const sendRegisterInfo = ({ tenant_id, email, first_name, last_name, phone }) => {
   const p = new Promise((res, rej) => {
-    // axios.post(`${REGISTER_MICROSERVICE}/post_tenant_info`, { staff_id, email, name, corporation_id, staff_title, temp_pass, accessToken }, authHeaders())
-    //   .then((data) => {
-    //     // once we have the response, only then do we dispatch an action to Redux
-    //     console.log(data)
-    //     res(data.data.message)
-    //   })
-    //   .catch((err) => {
-    //     rej(err)
-    //   })
+    axios.post(`${CONTRACTING_MICROSERVICE}/register_tenant`, { tenant_id, email, first_name, last_name, phone, }, authHeaders())
+      .then((data) => {
+        // once we have the response, only then do we dispatch an action to Redux
+        console.log(data)
+        res(data.data.message)
+      })
+      .catch((err) => {
+        rej(err)
+      })
   })
   return p
 }
